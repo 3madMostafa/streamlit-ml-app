3795ee524ba954aaaa4673b3f61ee7c887e5ca86
3madMostafa <118833050+3madMostafa@users.noreply.github.com>
2024-10-19 04:54:38 +0300

    Update ML_Flow.py

--- ML_Flow.py
+++ ML_Flow.py
@@ -9,6 +9,10 @@ import streamlit as st
 def train_and_log_model():
     st.title("MLflow Model Training & Logging")
 
+    st.markdown("""
+    **Hello everyone!** This part we will explain it locally.
+    """)
+
     # Load dataset
     try:
         df = pd.read_csv('modified_subset_data.csv')
